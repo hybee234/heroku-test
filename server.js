c1e5f2f5c09ccaae4524dc6b9a8395b5bac369ac
Hybee <hybee234@outlook.com>
2023-11-02 20:03:26 +1100

    troubleshooting

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3001
-
-const secretKey = process.env.SEUPER_SECRET_KEY
+// const PORT = process.env.PORT || 3001
+// const secretKey = process.env.SUPER_SECRET_KEY
 
 app.get('/', (req, res) => res.send ('Hello World'));
 
-
-
 app.listen (3001, () => {
-Console.log ('App is up and running & listening')
+console.log ('App is up and running & listening')
 });
