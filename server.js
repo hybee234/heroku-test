350d8ef8609897de2d14abe3216ff44849397acf
Hybee <hybee234@outlook.com>
2023-11-02 20:25:17 +1100

    test updating more

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 3001
 // const secretKey = process.env.SUPER_SECRET_KEY
 
-app.get('/', (req, res) => res.send ('Hello World'));
+app.get('/', (req, res) => res.send ('Hello World - Hopefully this goes to Heroku successfully!'));
 
 app.listen (PORT, () => {
-console.log ('App is up and running & listening')
+console.log ('App is up and running & listening http://localhost:3001/')
 });
